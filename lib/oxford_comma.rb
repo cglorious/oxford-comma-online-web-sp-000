0307d8f14cac51960bd255480dfa250f656811b4
Catherine Gloria <catherinegloria@gmail.com>
2020-09-04 10:09:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,14 @@
 def oxford_comma(array)
   string = ""
   array.each do |element|
-    string << element + ", "
+    until element == array[-2]
+      string << element + ", "
+    end
   end
+  last_element = array.to_s
+  string << ", and" + last_element
   string
 end
 
-animals = ["cats", "dogs"]
+animals = ["cats", "dogs", "birds"]
 oxford_comma(animals)
