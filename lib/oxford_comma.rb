84b77618ed0e064f1f16c6b367a44794a683a4ec
Catherine Gloria <catherinegloria@gmail.com>
2020-09-04 10:30:07 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -14,23 +14,10 @@ def oxford_comma(array)
 
 end
 
-animals = ["cats", "dogs", "gerbals", "birds"]
-oxford_comma(animals)
-
-def one_el(array)
-  if array.length == 1
-    array.join
-  end
-end
-
 drinks = ["matcha"]
-one_el(drinks)
-
-def two_el(array)
-  if array.length == 2
-    array.join(" and ")
-  end
-end
-
 hobbies = ["sing", "code"]
-two_el(hobbies)
+animals = ["cats", "dogs", "gerbals", "birds"]
+
+oxford_comma(drinks)
+oxford_comma(hobbies)
+oxford_comma(animals)
